605421fbc130be4d56916b9be436c3e74a421b98
shailensukul <shailensukul@gmail.com>
2014-08-10 11:08:53 +1000

    Export Fields for O365

---
+++ Presentation.Taxonomy.Demo/PS/ExportFieldO365.ps1
@@ -0,0 +1,88 @@
+﻿#######################
+#
+# Migrate Site Columns by Custom Group Name
+#
+# "Elise", a series of scripts to migrate O365 SharePoint Online Assets across O365 instances
+#
+# Copyright 2014, John Wefler, Rightpoint Consulting, LLC.
+#
+#######################
+
+# Change the following to reflect your environments
+
+# 1) Source Site
+$sUrl = "https://wefnetOrig.sharepoint.com/"
+$sAdmin = "wef@wefnetOrig.onmicrosoft.com"
+$sPwd = "xxxxxxxxxxx"
+
+# 2) Destination Site
+$dUrl = "https://wefnetDest.sharepoint.com/"
+$dAdmin = "wef@WEFNETDEST.onmicrosoft.com"
+$dPwd = "xxxxxxxxxxx"
+
+# 3) What Site Column Group do you want to synchronize?
+$sGroupName = "Wef Custom"
+
+## Stop here
+$lcid = "1033"
+
+$sSecurePwd = ConvertTo-SecureString $sPwd -AsPlainText -Force
+$dSecurePwd = ConvertTo-SecureString $dPwd -AsPlainText -Force
+
+# these aren't required for the script to run, but help to develop
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.dll"
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Runtime.dll"
+# doh
+Add-Type -Path "c:\Program Files\Common Files\microsoft shared\Web Server Extensions\15\ISAPI\Microsoft.SharePoint.Client.Taxonomy.dll"
+
+# connect/authenticate to SharePoint Online and get ClientContext object.. 
+$sCtx = New-Object Microsoft.SharePoint.Client.ClientContext($sUrl)
+$sCredentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($sAdmin, $sSecurePwd)
+$sCtx.Credentials = $sCredentials
+
+$dCtx = New-Object Microsoft.SharePoint.Client.ClientContext($dUrl)
+$dCredentials = New-Object Microsoft.SharePoint.Client.SharePointOnlineCredentials($dAdmin, $dSecurePwd)
+$dCtx.Credentials = $dCredentials
+
+$continue = 0
+
+if (!$dCtx.ServerObjectIsNull.Value)
+{
+    Write-Host "Connected to DESTINATION SharePoint Online site: " $dCtx.Url "" -ForegroundColor Green
+
+    $continue = 1
+}
+ 
+if (!$sCtx.ServerObjectIsNull.Value -and $continue -eq 1) 
+{ 
+    Write-Host "Connected to the SOURCE SharePoint Online site: " $sCtx.Url "" -ForegroundColor Green
+    $sSite = $sCtx.Web
+    $sCols = $sSite.AvailableFields
+    $sCtx.Load($sCols)
+    $sCtx.ExecuteQuery()
+
+    Write-Host "Found" $sCols.Count "Site Columns" -ForegroundColor Cyan
+
+    foreach($sCol in $sCols)
+    {
+        if($sCol.Group -eq $sGroupName)
+        {
+            Write-Host ".........Column found:" $sCol.StaticName "" -ForegroundColor Cyan
+            
+            #convert to XML object for future manipulation of nodes
+            [xml]$sColXML = $sCol.SchemaXml
+
+            $newCol = $sColXML.OuterXml.ToString()
+
+            $options = [Microsoft.SharePoint.Client.AddFieldOptions]::AddFieldToDefaultView
+
+            $dField = $dCtx.Web.Fields.AddFieldAsXml($newCol, $true, $options)
+            
+            $dCtx.Load($dCtx.Web)
+            $dCtx.ExecuteQuery()
+        }
+    }
+}
+
+$dCtx.Dispose()
+$sCtx.Dispose()
---
+++ Presentation.Taxonomy.Demo/Resources/js/Staff.js
@@ -0,0 +1,43 @@
+﻿if (SUKUL == undefined) var SUKUL;
+if (!SUKUL) SUKUL = {};
+if (SUKUL.Reports == undefined) SUKUL.Reports = {};
+
+if (SUKUL.Reports.Staff == undefined) {
+    SUKUL.Reports.Staff = function () {
+        var self = this;
+
+        self.Execute = function () {
+            var clientContext = new SP.ClientContext("/sites/demo");
+            var list = clientContext.get_web().get_lists().getByTitle('Staff');
+
+            var camlQuery = new SP.CamlQuery();
+            camlQuery.set_viewXml("<View><Query><Where><Eq><FieldRef Name='IsStaffRoomLeader' /><Value Type='Text'>TRUE</Value></Eq></Where>" +
+                "<ViewFields><FieldRef Name='FullName' /><FieldRef Name='EmailAddress' /></ViewFields></Query><RowLimit>100</RowLimit></View>");
+
+            self.listItems = list.getItems(camlQuery);
+
+            clientContext.load(self.listItems);
+            clientContext.executeQueryAsync(Function.createDelegate(this, self.dataLoaded),
+                Function.createDelegate(this, self.onRequestFailed));
+        };
+
+        self.dataLoaded = function () {
+            var listItemEnumerator = self.listItems.getEnumerator();
+            var ul = $("<ul style='list-style-type: none; padding-left: 0px;' class='cbs-List'>");
+            while (listItemEnumerator.moveNext()) {
+                var listItem = listItemEnumerator.get_current();
+
+                var htmlStr = "<li style='display: inline;'><div style='width: 320px; display: table; margin-bottom: 10px; margin-top: 5px;'>";
+                htmlStr += "<a href='mailto:" + listItem.get_item('EmailAddress') + "'>" + listItem.get_item('FullName1') + "</a>";
+                htmlStr += "</li>";
+                ul.append($(htmlStr));
+            }
+            ul.append("</ul>");
+            $("#divContentContainer").html(ul);
+        };
+
+        self.onRequestFailed = function (sender, args) {
+            alert('Error: ' + args.get_message());
+        }
+    }
+}
